045b3dfdda18ee4b452606a2fd04794846827e9a
Shengan Zheng <venero05@163.com>
2018-05-28 19:31:45 -0700

    Minor

--- fs/nova/debug.h
+++ fs/nova/debug.h
@@ -12,7 +12,7 @@
 #define MODE_KEEP_STAT 0
 #define MODE_KEEP_STAT_VPMEM
 
-#define MODE_FORE_ALLOC 1
+#define MODE_FORE_ALLOC 0
 #define MODE_FORE_PMEM 1
 #define MODE_FORE_BAL 0
 #define MODE_BACK_MIG 1
