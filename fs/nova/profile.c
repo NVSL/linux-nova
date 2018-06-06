7c56c1da0036932911df4b44f0d7433927e7795d
Shengan Zheng <venero05@163.com>
2018-06-06 13:22:56 -0700

    Configure for nv-01

--- fs/nova/profile.c
+++ fs/nova/profile.c
@@ -278,10 +278,10 @@ int nova_give_advise(struct nova_sb_info *sbi) {
     bitw = most_sig_lbit(total_write);
     i = bitr - bitw;
     sbi->stat->adv = i;
-    adv = 70 + i*5;
+    adv = 80 + i*5;
     if (adv>90) adv = 90;
     if (adv<10) adv = 10;
-    if (MODE_USE_DYN_THRES) MIGRATION_DOWN_PMEM_PERC = adv>70 ? adv:70;
+    if (MODE_USE_DYN_THRES) MIGRATION_DOWN_PMEM_PERC = adv>80 ? adv:80;
     return 0;
 }
 
